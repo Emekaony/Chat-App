848a7d55275f51c708ea4722b64f7a2c64a450e8
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2022-10-05 23:53:21 -0700

    Initial commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by SHAdON . on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
